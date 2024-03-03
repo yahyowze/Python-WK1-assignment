aa0b767cca6310a80b555a0de141b4d6ef8b3611
yahya Etho <https://github.com/yahyowze/Web-Dev-Work2.git>
2024-03-03 14:53:13 -0800

    first commit

---
+++ work1.py
@@ -0,0 +1,5 @@
+name = input('please enter your name: ')
+age = input('what is your age: ')
+location = input('where are you: ')
+
+print('Hello, my name is', name, 'i am', age, 'years old', 'and i live in', location)
